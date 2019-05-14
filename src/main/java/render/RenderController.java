09471f5bfcb82cc4f4d581caaa60d9a38231a62c
Jeff Schwartz <jeff@schwartz-tribe.com>
2019-05-14 17:27:58 -0400

    Research (#1)
    * Research branch with injection of FM stuff
    
    * Hack the configuration for freemarker
    
    * Updated with manual render of FreeMarker template

--- src/main/java/render/RenderController.java
+++ src/main/java/render/RenderController.java
@@ -1,26 +1,60 @@
 package render;
 
 
+import io.micronaut.core.io.Writable;
 import io.micronaut.core.util.CollectionUtils;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 import io.micronaut.views.ModelAndView;
+import io.micronaut.views.freemarker.FreemarkerViewsRenderer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.io.StringWriter;
 
 @Secured(SecurityRule.IS_ANONYMOUS)
 @Controller("/")
 public class RenderController {
+
+    @Inject
+    private FreemarkerViewsRenderer freemarkerViewsRenderer;
+
+    public RenderController(FreemarkerViewsRenderer freemarkerViewsRenderer) {
+        this.freemarkerViewsRenderer = freemarkerViewsRenderer;
+        log.info("GOT AN FVR: {}", freemarkerViewsRenderer);
+    }
+
     private static final Logger log = LoggerFactory.getLogger(RenderController.class);
 
-    @Get("/home")
+    @Get("/homeManualRender")
+    @Produces(value=MediaType.TEXT_HTML)
     @Secured(SecurityRule.IS_ANONYMOUS)
-    public ModelAndView displayHomePage() {
+    public String manualRenderHomePage() {
+        ModelAndView mav = new ModelAndView("pages/home", CollectionUtils.mapOf("loggedIn", true, "username", "sdelamo"));
+
+        Writable w = freemarkerViewsRenderer.render("pages/home", mav.getModel());
+
+        StringWriter sw = new StringWriter();
+        try {
+            w.writeTo(sw);
+        } catch (IOException e) {
+            log.error("Error writing template", e);
+        }
+
+        return sw.toString();
+    }
+
+    @Get("/home")
+    @Produces(value=MediaType.TEXT_HTML)
+    @Secured(SecurityRule.IS_ANONYMOUS)
+    public ModelAndView mnRenderHomePage() {
         ModelAndView mav = new ModelAndView("pages/home", CollectionUtils.mapOf("loggedIn", true, "username", "sdelamo"));
         return mav;
     }
-
 }
--- src/main/java/render/StreamLambdaHandler.java
+++ src/main/java/render/StreamLambdaHandler.java
@@ -4,12 +4,15 @@ import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.model.*;
 import com.amazonaws.services.lambda.runtime.*;
 import io.micronaut.function.aws.proxy.MicronautLambdaContainerHandler;
+import org.apache.commons.io.IOUtils;
+
 import java.io.*;
 
 public class StreamLambdaHandler implements RequestStreamHandler {
     private static MicronautLambdaContainerHandler handler; // <1>
     static {
         try {
+//            System.setProperty("micronaut.environments", "function");
             handler = MicronautLambdaContainerHandler.getAwsProxyHandler();
         } catch (ContainerInitializationException e) {
             // if we fail here. We re-throw the exception to force another cold start
@@ -21,6 +24,13 @@ public class StreamLambdaHandler implements RequestStreamHandler {
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
             throws IOException {
-        handler.proxyStream(inputStream, outputStream, context); // <2>
+
+        String in = IOUtils.toString(inputStream);
+
+        System.out.println("RAW.IN: " + in);
+
+        InputStream is2 = IOUtils.toInputStream(in);
+
+        handler.proxyStream(is2, outputStream, context); // <2>
     }
 }
