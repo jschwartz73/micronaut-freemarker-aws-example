9620d9a8e048c1733f156d9040cf94b41d911f9e
Jeff Schwartz <jeff@schwartz-tribe.com>
2019-05-14 17:19:04 -0400

    Updated with manual render of FreeMarker template

--- src/main/java/render/RenderController.java
+++ src/main/java/render/RenderController.java
@@ -1,8 +1,7 @@
 package render;
 
 
-import freemarker.template.Configuration;
-import io.micronaut.context.env.Environment;
+import io.micronaut.core.io.Writable;
 import io.micronaut.core.util.CollectionUtils;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
@@ -11,39 +10,51 @@ import io.micronaut.http.annotation.Produces;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 import io.micronaut.views.ModelAndView;
-import io.micronaut.views.ViewsConfiguration;
-import io.micronaut.views.freemarker.FreemarkerFactory;
-import io.micronaut.views.freemarker.FreemarkerViewsRendererConfiguration;
-import io.micronaut.views.freemarker.MyFreemarkerFactoryHelper;
+import io.micronaut.views.freemarker.FreemarkerViewsRenderer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.io.IOException;
+import java.io.StringWriter;
 
 @Secured(SecurityRule.IS_ANONYMOUS)
 @Controller("/")
 public class RenderController {
-    @Inject
-    private Environment environment;
 
-    public RenderController(Environment environment) {
-        this.environment = environment;
-        log.info("GOT AN ENVIRONMENT");
+    @Inject
+    private FreemarkerViewsRenderer freemarkerViewsRenderer;
+
+    public RenderController(FreemarkerViewsRenderer freemarkerViewsRenderer) {
+        this.freemarkerViewsRenderer = freemarkerViewsRenderer;
+        log.info("GOT AN FVR: {}", freemarkerViewsRenderer);
     }
 
     private static final Logger log = LoggerFactory.getLogger(RenderController.class);
 
+    @Get("/homeManualRender")
+    @Produces(value=MediaType.TEXT_HTML)
+    @Secured(SecurityRule.IS_ANONYMOUS)
+    public String manualRenderHomePage() {
+        ModelAndView mav = new ModelAndView("pages/home", CollectionUtils.mapOf("loggedIn", true, "username", "sdelamo"));
+
+        Writable w = freemarkerViewsRenderer.render("pages/home", mav.getModel());
+
+        StringWriter sw = new StringWriter();
+        try {
+            w.writeTo(sw);
+        } catch (IOException e) {
+            log.error("Error writing template", e);
+        }
+
+        return sw.toString();
+    }
+
     @Get("/home")
     @Produces(value=MediaType.TEXT_HTML)
     @Secured(SecurityRule.IS_ANONYMOUS)
-    public ModelAndView displayHomePage() {
-        MyFreemarkerFactoryHelper myFreemarkerFactoryHelper = new MyFreemarkerFactoryHelper();
-
-        Configuration config = myFreemarkerFactoryHelper.getConfiguration(environment);
-        log.info("CONFIG: {}", config);
-
+    public ModelAndView mnRenderHomePage() {
         ModelAndView mav = new ModelAndView("pages/home", CollectionUtils.mapOf("loggedIn", true, "username", "sdelamo"));
         return mav;
     }
-
 }
--- src/main/java/render/StreamLambdaHandler.java
+++ src/main/java/render/StreamLambdaHandler.java
@@ -4,6 +4,8 @@ import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.model.*;
 import com.amazonaws.services.lambda.runtime.*;
 import io.micronaut.function.aws.proxy.MicronautLambdaContainerHandler;
+import org.apache.commons.io.IOUtils;
+
 import java.io.*;
 
 public class StreamLambdaHandler implements RequestStreamHandler {
@@ -22,6 +24,13 @@ public class StreamLambdaHandler implements RequestStreamHandler {
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
             throws IOException {
-        handler.proxyStream(inputStream, outputStream, context); // <2>
+
+        String in = IOUtils.toString(inputStream);
+
+        System.out.println("RAW.IN: " + in);
+
+        InputStream is2 = IOUtils.toInputStream(in);
+
+        handler.proxyStream(is2, outputStream, context); // <2>
     }
 }
